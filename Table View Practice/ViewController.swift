60964dbf12897b0344e9596442936da8417b0350
Dancilia Kaye-Cee Harmon <dancilia.harmon@techconsulting.tech>
2024-12-24 15:09:15 -0500

    Initial Commit

---
+++ Table View Practice/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Table View Practice
+//
+//  Created by Dancilia Harmon   on 12/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let fruits = ["Apple", "Banana", "Cherry", "Grape", "Orange", "Strawberry"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = fruits[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedFruit = fruits[indexPath.row]
+        
+        let alert = UIAlertController(title: "You selected", message: selectedFruit, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
+
